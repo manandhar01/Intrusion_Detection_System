88ac2ed5c66de580fc9fe621974c85520e4bad7c
Bibek Manandhar <manandhar.bibek01@gmail.com>
2022-11-17 15:26:44 +0545

    csv to json

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+// const fs = require("fs");
 const app = express();
+const csvtojson = require("csvtojson");
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -16,141 +17,17 @@ app.use(bodyParser.json());
 
 const port = 5000;
 
-let data = [
-    {
-        a: {
-            x: 3,
-            y: 9,
-        },
-        b: 2,
-        c: 3,
-    },
-    {
-        a: {
-            x: 0,
-            y: 3,
-        },
-        b: 5,
-        c: 6,
-    },
-    {
-        a: {
-            x: 4,
-            y: 8,
-        },
-        b: 8,
-        c: 9,
-    },
-    {
-        a: {
-            x: 0,
-            y: 1,
-        },
-        b: 5,
-        c: 6,
-    },
-    {
-        a: {
-            x: 3,
-            y: 9,
-        },
-        b: 2,
-        c: 3,
-    },
-    {
-        a: {
-            x: 0,
-            y: 3,
-        },
-        b: 5,
-        c: 6,
-    },
-    {
-        a: {
-            x: 4,
-            y: 8,
-        },
-        b: 8,
-        c: 9,
-    },
-    {
-        a: {
-            x: 0,
-            y: 1,
-        },
-        b: 5,
-        c: 6,
-    },
-    {
-        a: {
-            x: 3,
-            y: 9,
-        },
-        b: 2,
-        c: 3,
-    },
-    {
-        a: {
-            x: 0,
-            y: 3,
-        },
-        b: 5,
-        c: 6,
-    },
-    {
-        a: {
-            x: 4,
-            y: 8,
-        },
-        b: 8,
-        c: 9,
-    },
-    {
-        a: {
-            x: 0,
-            y: 1,
-        },
-        b: 5,
-        c: 6,
-    },
-    {
-        a: {
-            x: 3,
-            y: 9,
-        },
-        b: 2,
-        c: 3,
-    },
-    {
-        a: {
-            x: 0,
-            y: 3,
-        },
-        b: 5,
-        c: 6,
-    },
-    {
-        a: {
-            x: 4,
-            y: 8,
-        },
-        b: 8,
-        c: 9,
-    },
-    {
-        a: {
-            x: 0,
-            y: 1,
-        },
-        b: 5,
-        c: 6,
-    },
-];
-
-data = JSON.stringify(data);
-
 app.get("/data", (req, res) => {
-    res.json(data);
+    csvtojson()
+        .fromFile("./test.csv")
+        .then((data) => {
+            let columns = [];
+            cols = Object.keys(data[0]);
+            cols.forEach((col) => {
+                columns.push({ accessorKey: col, header: col });
+            });
+            res.json(JSON.stringify({ data, columns }));
+        });
 });
 
 app.listen(port, () => {
