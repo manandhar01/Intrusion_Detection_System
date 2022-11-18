c36133e9172478fc27dddff3ba5ba908263f4c20
Bibek Manandhar <manandhar.bibek01@gmail.com>
2022-11-18 18:37:18 +0545

    fileWatch implemented

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const fs = require("fs");
+const fs = require("fs");
 const app = express();
 const csvtojson = require("csvtojson");
 
@@ -17,17 +17,28 @@ app.use(bodyParser.json());
 
 const port = 5000;
 
+let fileChanged = true;
+
+fs.watchFile("test.csv", () => {
+    fileChanged = true;
+});
+
 app.get("/data", (req, res) => {
-    csvtojson()
-        .fromFile("./test.csv")
-        .then((data) => {
-            let columns = [];
-            cols = Object.keys(data[0]);
-            cols.forEach((col) => {
-                columns.push({ accessorKey: col, header: col });
+    if (fileChanged) {
+        csvtojson()
+            .fromFile("./test.csv")
+            .then((data) => {
+                let columns = [];
+                cols = Object.keys(data[0]);
+                cols.forEach((col) => {
+                    columns.push({ accessorKey: col, header: col });
+                });
+                fileChanged = false;
+                res.json(JSON.stringify({ data, columns, fileChanged: true }));
             });
-            res.json(JSON.stringify({ data, columns }));
-        });
+    } else {
+        res.json(JSON.stringify({ fileChanged: false }));
+    }
 });
 
 app.listen(port, () => {
