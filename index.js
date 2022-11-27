70ccecc857cce9e84c3861583c224fd544a6a69e
Abhinandan Shrestha <shtabhi@gmail.com>
2022-11-27 20:47:50 +0545

    post request

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const cors=require('cors');
 const server = http.createServer(app);
 const fs = require('fs');
 const csvtojson = require("csvtojson");
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
 
 const io = new Server(server,{
@@ -33,6 +36,12 @@ io.on("connection",(socket)=>{
     });
     console.log(`User connected: ${socket.id}`);
 });
+
+
+app.post('/post',(req,res)=>{
+    console.log(req.body);
+    res.json(req.body);
+})
 
 server.listen(3001,()=>{
     console.log("Listening at 3001")
