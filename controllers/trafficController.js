f06987a47247f2d346c05c7b1c37228ff5cf0722
Bibek Manandhar <manandhar.bibek01@gmail.com>
2023-02-15 12:55:05 +0545

    some changes

--- controllers/trafficController.js
+++ controllers/trafficController.js
@@ -22,29 +22,6 @@ const getTrafficData = (req, res) => {
 };
 
 const watchCaptureFile = () => {
-    // let prevData = "";
-    // console.log(`Watching ${filename} for changes`);
-    // fs.watchFile(filename, (curr, prev) => {
-    //     fs.readFile(filename, "utf-8", (err, data) => {
-    //         if (err) {
-    //             console.error(err);
-    //         } else {
-    //             if (prevData !== data) {
-    //                 const newData = data.substring(prevData.length);
-    //                 prevData = data;
-    //                 const formattedData = formatData(newData);
-    //                 predictMany(formattedData).then((result) => {
-    //                     if (result) {
-    //                         console.log("Prediction complete");
-    //                     } else {
-    //                         console.log("could not predict");
-    //                     }
-    //                 });
-    //             }
-    //         }
-    //     });
-    // });
-
     console.log(`Watching ${filename} for changes`);
     const watchProcess = spawn(
         `python "AI/omniglot-fewshot/predict_captured.py"`,
